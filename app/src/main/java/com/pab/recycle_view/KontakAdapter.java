c244aeeb9c38676ced4e2777e9b937fcfa050738
Dede-Husen <adehusen@gmail.com>
2024-04-23 14:22:06 +0700

    update penambahan action call

--- app/src/main/java/com/pab/recycle_view/KontakAdapter.java
+++ app/src/main/java/com/pab/recycle_view/KontakAdapter.java
@@ -3,7 +3,6 @@ package com.pab.recycle_view;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
